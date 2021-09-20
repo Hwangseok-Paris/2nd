39864241a9c1fdda3a5a72c672b9bb4375e972cc
Hwangseok-Paris <hwangseok.paris@gmail.com>
2021-09-20 17:34:33 +0900

    210920 1734 : commit  - 시계 박스 영역 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,10 @@ const Footer = () => {
                 <div className="footer_contents">
                     Since 2021-09-15
                 </div>
-                <div className="clock">
-                    <Clock format={'dddd Do MMM HH:mm:ss'} ticking='true' timezone={'KST'} />
+                <div className="clockBox">
+                    <div className="clock">
+                        <Clock format={'dddd Do MMM HH:mm:ss'} ticking='true' timezone={'KST'} />
+                    </div>
                 </div>
             </footer>
         </div>
